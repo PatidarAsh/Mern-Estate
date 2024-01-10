66f82e861b12d111720b86de74ecd628d7b30922
PatidarAsh <113142513+PatidarAsh@users.noreply.github.com>
2024-01-10 17:56:06 -0500

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
