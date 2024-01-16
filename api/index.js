ae2660c38faee94b28557d28a5662b488ae5a3b3
PatidarAsh <113142513+PatidarAsh@users.noreply.github.com>
2024-01-15 19:55:31 -0500

    adding signup api's

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express, { application } from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 mongoose.connect("mongodb://localhost:27017/Mern-Estate").then (() =>{
     console.log("conncted to database");
@@ -10,9 +11,12 @@ mongoose.connect("mongodb://localhost:27017/Mern-Estate").then (() =>{
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, ()=>{
     console.log('server is running on port 3000!');
     }
 );
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
