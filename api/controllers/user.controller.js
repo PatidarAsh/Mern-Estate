c10c0f283fa487c178a2501f80cd0b5e0fab03dc
PatidarAsh <113142513+PatidarAsh@users.noreply.github.com>
2024-01-26 14:24:15 -0500

    complete show user listing functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from 'bcryptjs';
+import Listing from '../models/listing.model.js';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 
@@ -52,3 +53,16 @@ export const updateUser = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const getUserListings = async(req, res, next) => {
+    if(req.user.id === req.params.id) {
+      try{
+        const listings = await Listing.find({userRef: req.params.id})
+        res.status(200).json(listings)
+      }catch(error){
+        next(error)
+      }
+    }else{
+      return next(errorHandler(401, 'You can view your own listing only!'))
+    }
+  }
