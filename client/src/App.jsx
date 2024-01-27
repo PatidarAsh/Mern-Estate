e59be323a19c1a5164cedbd72298a3dea114dc49
PatidarAsh <113142513+PatidarAsh@users.noreply.github.com>
2024-01-27 18:59:39 -0500

    fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
+import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing'
