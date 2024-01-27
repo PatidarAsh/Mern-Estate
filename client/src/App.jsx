6c6aefd3c8324f85fe7e26e40278cb4e284329de
PatidarAsh <113142513+PatidarAsh@users.noreply.github.com>
2024-01-26 19:32:32 -0500

    image slider for listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,8 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import PrivateRoute from './components/privateRoute';
 import CreateListing from './pages/CreateListing';
-import UpdateListing from './pages/updateListing';
+import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing'
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
+      <Route path="/listing/:listingId" element={<Listing />} />
       <Route  element={<PrivateRoute />} >
       <Route path="/profile" element={<Profile />} />
       <Route path="/create-listing" element={<CreateListing />} />
