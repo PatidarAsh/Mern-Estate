6a2eb0f4a5de8d1b8bd92415be01fc4ea521eaa5
PatidarAsh <113142513+PatidarAsh@users.noreply.github.com>
2024-01-27 19:03:46 -0500

    deleted file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,21 +16,24 @@ import Search from './pages/Search';
 function App() {
   return (
     <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path='/search' element={<Search />} />
-      <Route path="/listing/:listingId" element={<Listing />} />
-      <Route  element={<PrivateRoute />} >
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/create-listing" element={<CreateListing />} />
-      <Route path="/update-listing/:listingId" element={<UpdateListing />} />
-      </Route>
-     
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
--- client/src/components/privateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Outlet, Navigate } from 'react-router-dom';
-
-function privateRoute() {
-    const { currentUser } = useSelector((state) => state.user);
-    return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
-}
-
-export default privateRoute
